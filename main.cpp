7caf27ad267dd36272a3660534c4161e60e516f8
Jon Wu <icoolsea@gmail.com>
2016-06-07 19:08:15 +0800

    create

---
+++ main.cpp
@@ -0,0 +1,14 @@
+
+#include "vlc_on_qt.h"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    Player p;
+    p.resize(640,480);
+  //  p.playFile("rtp://@:2626"); // Replace with what you want to play
+    p.playFile("file://home/jon/qtest/dsa.ts");
+    p.show();
+    return a.exec();
+}
